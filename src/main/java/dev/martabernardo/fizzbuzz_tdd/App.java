30fa1e58c82d4274316bd854b9f5056dbae48062
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-25 17:33:44 +0100

    initial branch commit with 1 to 100 count

--- src/main/java/dev/martabernardo/fizzbuzz_tdd/App.java
+++ src/main/java/dev/martabernardo/fizzbuzz_tdd/App.java
@@ -12,6 +12,9 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int i = 0; i < 100; i++){        
+            System.out.println(i);
+        }
+        System.out.println("Has completado la cuenta de cien");
     }
 }
