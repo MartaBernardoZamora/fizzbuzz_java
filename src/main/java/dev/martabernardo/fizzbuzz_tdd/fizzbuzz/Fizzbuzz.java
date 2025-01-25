6b1c1e07a032b4412851214eed5e797fc68c4e21
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-25 17:02:38 +0100

    buzz case implemented with ok status

--- src/main/java/dev/martabernardo/fizzbuzz_tdd/fizzbuzz/Fizzbuzz.java
+++ src/main/java/dev/martabernardo/fizzbuzz_tdd/fizzbuzz/Fizzbuzz.java
@@ -4,7 +4,10 @@ public class Fizzbuzz {
 
     public String checkNumber(int num) {
         boolean isDivisibleBy3 = num % 3 == 0;
+        boolean isDivisibleBy5 = num % 5 == 0;
+
         if(isDivisibleBy3) return "Fizz";
+        if(isDivisibleBy5) return "Buzz";
         return "null";
     }
 
