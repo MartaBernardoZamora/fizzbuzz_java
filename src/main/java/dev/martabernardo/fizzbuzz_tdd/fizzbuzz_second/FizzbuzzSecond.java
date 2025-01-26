2b1726b9ccf222a5ea361fb3546c0c23dad02871
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-26 19:07:27 +0100

    test contains 3 ok status

--- src/main/java/dev/martabernardo/fizzbuzz_tdd/fizzbuzz_second/FizzbuzzSecond.java
+++ src/main/java/dev/martabernardo/fizzbuzz_tdd/fizzbuzz_second/FizzbuzzSecond.java
@@ -1,13 +1,14 @@
 package dev.martabernardo.fizzbuzz_tdd.fizzbuzz_second;
 
-//import dev.martabernardo.fizzbuzz_tdd.fizzbuzz.Fizzbuzz;
+import dev.martabernardo.fizzbuzz_tdd.fizzbuzz.Fizzbuzz;
 
 public class FizzbuzzSecond {
     public String secondCheckNumber(int num) {
-        //String checkNumberResult = new Fizzbuzz().checkNumber(num);
-
-        //System.out.println(checkNumberResult);
-        return "Fizz";
+        String checkNumberResult = new Fizzbuzz().checkNumber(num);
+        if(checkNumberResult.contains("3")){
+            return "Fizz";
+        }
+        return checkNumberResult;
     }
 
 }
