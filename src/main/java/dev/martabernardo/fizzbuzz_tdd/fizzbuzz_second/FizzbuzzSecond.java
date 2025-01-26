3c9153a6ac8f125625734e7815f2ff7134db2a7d
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-26 19:27:51 +0100

    second stage complete to instructions

--- src/main/java/dev/martabernardo/fizzbuzz_tdd/fizzbuzz_second/FizzbuzzSecond.java
+++ src/main/java/dev/martabernardo/fizzbuzz_tdd/fizzbuzz_second/FizzbuzzSecond.java
@@ -7,9 +7,9 @@ public class FizzbuzzSecond {
         String checkNumberResult = new Fizzbuzz().checkNumber(num);
         boolean contains3=checkNumberResult.contains("3");
         boolean contains5=checkNumberResult.contains("5");
-        boolean contains5and3=checkNumberResult.contains("5")&&checkNumberResult.contains("3");
+        //boolean contains5and3=checkNumberResult.contains("5")&&checkNumberResult.contains("3");
 
-        if(contains5and3) return "Fizzbuzz";
+        //if(contains5and3) return "Fizzbuzz";
         if(contains3) return "Fizz";
         if(contains5) return "Buzz";
         return checkNumberResult;
